ad50d97070eb79a3752d893d7794de19c59e6490
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2025-12-04 17:08:16 -0300

    update age

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,45 +17,41 @@ export const About = () => {
         </span>
       </h1>
       <div className="mt-4 text-gray-400 mb-8 flex gap-4 flex-col">
-        <p>I&apos;m 27 years old and based in São Paulo, Brazil.</p>
+        <p>I&apos;m 28 years old and based in São Paulo, Brazil.</p>
         <p>
           I&apos;m a front-end designer with 10 years of experience developing
           and designing websites and web applications. I love creating clean,
-          user-friendly pages and making sure every project is visually appealing
-          and easy to use.
+          user-friendly pages and making sure every project is visually
+          appealing and easy to use.
         </p>
         <p>
-        Lately, I&apos;ve been passionate about web3, building seamless experiences that connect traditional and decentralized platforms.
+          Lately, I&apos;ve been passionate about web3, building seamless
+          experiences that connect traditional and decentralized platforms.
         </p>
       </div>
       <div className="flex justify-between items-center">
         <div className="flex gap-5 items-center">
           <Link
             className="text-gray-400 hover:text-blue-500 transition-all"
-            href="https://github.com/Vitagliano"
-          >
+            href="https://github.com/Vitagliano">
             <GithubIcon />
           </Link>
 
-         
           <Link
             className="text-gray-400 hover:text-blue-500 transition-all mt-0.5"
-            href="https://twitter.com/gabrielrvita"
-          >
+            href="https://twitter.com/gabrielrvita">
             <TwitterIcon />
           </Link>
 
           <Link
             className="text-gray-400 hover:text-blue-500 transition-all -mt-0.5"
-            href="https://www.linkedin.com/in/gabrielrvita/"
-          >
+            href="https://www.linkedin.com/in/gabrielrvita/">
             <LinkedInIcon />
           </Link>
         </div>
         <Link
           className="flex items-center text-white duration-150 bg-blue-600 hover:bg-blue-500 rounded-xl active:shadow-lg font-semibold text-sm flex gap-2 py-3 px-6"
-          href="/cv.pdf"
-        >
+          href="/cv.pdf">
           <DownloadIcon className="w-4 h-4" />
           My curriculum
         </Link>
